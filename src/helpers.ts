489815c13cfe3d0888667a717c094c1706c63f60
Data-Nexus <derek.m.meyer@gmail.com>
2023-11-14 09:13:18 -0500

    Added wrapped token

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,101 +1,91 @@
-import { Address, BigInt } from '@graphprotocol/graph-ts'
-import { User, Proposal, ProposalVote, RewardsClaimed, Governance } from '../generated/schema';
+import {Address, BigInt} from "@graphprotocol/graph-ts";
+import {User, Proposal, ProposalVote, RewardsClaimed, Governance} from "../generated/schema";
 
-export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
-type ProposalVoteId = string
+type ProposalVoteId = string;
 
-type RewardsClaimedId = string
+type RewardsClaimedId = string;
 
 export function getUser(address: Address): User {
-  let user = User.load(address.toHexString())
+  let user = User.load(address.toHexString());
   if (!user) {
-    user = new User(address.toHexString())
-    user.totalTokens = BigInt.fromI32(0)
-    user.totalStakedTokens = BigInt.fromI32(0)
-    user.votingPower = BigInt.fromI32(0)
-    user.proposingPower = BigInt.fromI32(0)
-    user.tokenVotingPower = BigInt.fromI32(0)
-    user.tokenProposingPower = BigInt.fromI32(0)
-    user.stakedTokenVotingPower = BigInt.fromI32(0)
-    user.stakedTokenProposingPower = BigInt.fromI32(0)
-    user.numberVotes = 0
+    user = new User(address.toHexString());
+    user.totalTokens = BigInt.fromI32(0);
+    user.totalStakedTokens = BigInt.fromI32(0);
+    user.votingPower = BigInt.fromI32(0);
+    user.proposingPower = BigInt.fromI32(0);
+    user.tokenVotingPower = BigInt.fromI32(0);
+    user.tokenProposingPower = BigInt.fromI32(0);
+    user.stakedTokenVotingPower = BigInt.fromI32(0);
+    user.stakedTokenProposingPower = BigInt.fromI32(0);
+    user.wrappedTokenVotingPower = BigInt.fromI32(0);
+    user.wrappedTokenProposingPower = BigInt.fromI32(0);
+    user.numberVotes = 0;
   }
 
   return user;
 }
 
 export function getProposal(proposalId: BigInt): Proposal {
-  let proposal = Proposal.load(proposalId.toString())
+  let proposal = Proposal.load(proposalId.toString());
   if (!proposal) {
-    proposal = new Proposal(proposalId.toString())
+    proposal = new Proposal(proposalId.toString());
   }
 
   return proposal;
 }
 
 export function getProposalVote(proposalId: BigInt, userAddress: Address): ProposalVote {
-  let proposalVoteId: ProposalVoteId = getProposalVoteId(proposalId, userAddress)
-  let proposalVote = ProposalVote.load(proposalVoteId)
+  let proposalVoteId: ProposalVoteId = getProposalVoteId(proposalId, userAddress);
+  let proposalVote = ProposalVote.load(proposalVoteId);
   if (!proposalVote) {
-    proposalVote = new ProposalVote(proposalVoteId)
+    proposalVote = new ProposalVote(proposalVoteId);
   }
 
   return proposalVote;
 }
 
 function getProposalVoteId(proposalId: BigInt, userAddress: Address): ProposalVoteId {
-  return proposalId.toString() + "-" + userAddress.toHexString()
+  return proposalId.toString() + "-" + userAddress.toHexString();
 }
 
-export function getRewardsClaimed(
-  userAddress: Address,
-  incentivesModule: String,
-  claimTimestamp: BigInt,
-): RewardsClaimed {
-  let userRewardId: RewardsClaimedId = getRewardsClaimedId(
-    userAddress,
-    incentivesModule,
-    claimTimestamp,
-  )
-  let userReward = RewardsClaimed.load(userRewardId)
+export function getRewardsClaimed(userAddress: Address, incentivesModule: String, claimTimestamp: BigInt): RewardsClaimed {
+  let userRewardId: RewardsClaimedId = getRewardsClaimedId(userAddress, incentivesModule, claimTimestamp);
+  let userReward = RewardsClaimed.load(userRewardId);
   if (!userReward) {
-    userReward = new RewardsClaimed(userRewardId)
+    userReward = new RewardsClaimed(userRewardId);
   }
 
   return userReward;
 }
 
-function getRewardsClaimedId(
-  userAddress: Address,
-  incentivesModule: String,
-  claimTimestamp: BigInt,
-): RewardsClaimedId {
-  return userAddress.toHexString() + "-" + incentivesModule + "-" + claimTimestamp.toString()
+function getRewardsClaimedId(userAddress: Address, incentivesModule: String, claimTimestamp: BigInt): RewardsClaimedId {
+  return userAddress.toHexString() + "-" + incentivesModule + "-" + claimTimestamp.toString();
 }
 
 export function changeUserTokenBalance(address: Address, amount: BigInt, add: boolean): void {
-  let user: User = getUser(address)
+  let user: User = getUser(address);
 
   if (add) {
-    user.totalTokens = user.totalTokens.plus(amount)
+    user.totalTokens = user.totalTokens.plus(amount);
   } else {
-    user.totalTokens = user.totalTokens.minus(amount)
+    user.totalTokens = user.totalTokens.minus(amount);
   }
 
-  user.save()
+  user.save();
 }
 
 export function changeUserStakedTokenBalance(address: Address, amount: BigInt, add: boolean): void {
-  let user: User = getUser(address)
+  let user: User = getUser(address);
 
   if (add) {
-    user.totalStakedTokens = user.totalStakedTokens.plus(amount)
+    user.totalStakedTokens = user.totalStakedTokens.plus(amount);
   } else {
-    user.totalStakedTokens = user.totalStakedTokens.minus(amount)
+    user.totalStakedTokens = user.totalStakedTokens.minus(amount);
   }
 
-  user.save()
+  user.save();
 }
 
 export function getGovernanceEntity(): Governance {
--- src/proposal.ts
+++ src/proposal.ts
@@ -1,82 +1,76 @@
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
-import { getUser, getProposal, getProposalVote, getGovernanceEntity } from "./helpers";
-import {
-  ProposalCreated,
-  VoteEmitted,
-  ProposalCanceled,
-  ProposalQueued,
-  ProposalExecuted
-} from "../generated/DydxGovernor/DydxGovernor"
-import { Proposal, ProposalVote, User, Governance } from "../generated/schema"
+import {Address, BigInt, Bytes} from "@graphprotocol/graph-ts";
+import {getUser, getProposal, getProposalVote, getGovernanceEntity} from "./helpers";
+import {ProposalCreated, VoteEmitted, ProposalCanceled, ProposalQueued, ProposalExecuted} from "../generated/DydxGovernor/DydxGovernor";
+import {Proposal, ProposalVote, User, Governance} from "../generated/schema";
 
 export function handleProposalCreated(event: ProposalCreated): void {
-  let proposal: Proposal = getProposal(event.params.id)
+  let proposal: Proposal = getProposal(event.params.id);
 
-  proposal.proposer = getUser(event.params.creator).id
-  proposal.targets = changetype<Bytes[]>(event.params.targets)
-  proposal.values = event.params.values
-  proposal.signatures = event.params.signatures
-  proposal.calldatas = event.params.calldatas
-  proposal.withDelegateCalls = event.params.withDelegatecalls
-  proposal.creationBlock = event.block.number
-  proposal.creationTime = event.block.timestamp
-  proposal.startBlock = event.params.startBlock
-  proposal.endBlock = event.params.endBlock
-  proposal.strategy = event.params.strategy
-  proposal.ipfsHash = event.params.ipfsHash
+  proposal.proposer = getUser(event.params.creator).id;
+  proposal.targets = changetype<Bytes[]>(event.params.targets);
+  proposal.values = event.params.values;
+  proposal.signatures = event.params.signatures;
+  proposal.calldatas = event.params.calldatas;
+  proposal.withDelegateCalls = event.params.withDelegatecalls;
+  proposal.creationBlock = event.block.number;
+  proposal.creationTime = event.block.timestamp;
+  proposal.startBlock = event.params.startBlock;
+  proposal.endBlock = event.params.endBlock;
+  proposal.strategy = event.params.strategy;
+  proposal.ipfsHash = event.params.ipfsHash;
 
-  proposal.save()
+  proposal.save();
 
   let governance: Governance = getGovernanceEntity();
-  governance.proposals = governance.proposals + BigInt.fromI32(1);
+  governance.proposals = governance.proposals.plus(BigInt.fromI32(1));
   governance.save();
 }
 
 export function handleProposalCanceled(event: ProposalCanceled): void {
-  let proposal: Proposal = getProposal(event.params.id)
+  let proposal: Proposal = getProposal(event.params.id);
 
-  proposal.cancellationBlock = event.block.number
-  proposal.cancellationTime = event.block.timestamp
+  proposal.cancellationBlock = event.block.number;
+  proposal.cancellationTime = event.block.timestamp;
 
-  proposal.save()
+  proposal.save();
 }
 
 export function handleProposalQueued(event: ProposalQueued): void {
-  let proposal: Proposal = getProposal(event.params.id)
+  let proposal: Proposal = getProposal(event.params.id);
 
-  proposal.executionETA = event.params.executionTime
-  proposal.queuedBlock = event.block.number
-  proposal.queuedTime = event.block.timestamp
+  proposal.executionETA = event.params.executionTime;
+  proposal.queuedBlock = event.block.number;
+  proposal.queuedTime = event.block.timestamp;
 
-  proposal.save()
+  proposal.save();
 }
 
 export function handleProposalExecuted(event: ProposalExecuted): void {
-  let proposal: Proposal = getProposal(event.params.id)
+  let proposal: Proposal = getProposal(event.params.id);
 
   proposal.executionETA = null;
-  proposal.executionBlock = event.block.number
-  proposal.executionTime = event.block.timestamp
+  proposal.executionBlock = event.block.number;
+  proposal.executionTime = event.block.timestamp;
 
-  proposal.save()
+  proposal.save();
 }
 
 export function handleVoteEmitted(event: VoteEmitted): void {
-  let userAddress: Address = event.params.voter
-  let proposalId: BigInt = event.params.id
-  let proposalVote: ProposalVote = getProposalVote(proposalId, userAddress)
+  let userAddress: Address = event.params.voter;
+  let proposalId: BigInt = event.params.id;
+  let proposalVote: ProposalVote = getProposalVote(proposalId, userAddress);
 
-  proposalVote.user = getUser(userAddress).id
-  proposalVote.proposal = getProposal(proposalId).id
-  proposalVote.support = event.params.support
-  proposalVote.votingPower = event.params.votingPower
+  proposalVote.user = getUser(userAddress).id;
+  proposalVote.proposal = getProposal(proposalId).id;
+  proposalVote.support = event.params.support;
+  proposalVote.votingPower = event.params.votingPower;
 
-  proposalVote.save()
+  proposalVote.save();
 
   // Ignore 0 weight votes
-  if(event.params.votingPower > BigInt.fromI32(0)) {
-    let user: User = getUser(event.params.voter)
-    user.numberVotes = user.numberVotes + 1
-    user.save()
+  if (event.params.votingPower > BigInt.fromI32(0)) {
+    let user: User = getUser(event.params.voter);
+    user.numberVotes = user.numberVotes + 1;
+    user.save();
   }
 }
